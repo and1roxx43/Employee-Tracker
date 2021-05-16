b461253931234ab19eb8dbc88bdfe4a08d3d3a9a
andrew quenette <and1roxx43@gmail.com>
2021-05-17 00:07:40 +1000

    Updated seeds.sql

--- seeds.sql
+++ seeds.sql
@@ -1,30 +1,4 @@
-DROP DATABASE IF EXISTS employee_trackerDB;
-CREATE DATABASE employee_trackerDB;
-
 USE employee_trackerDB;
-
-CREATE TABLE department (
-id INT NOT NULL AUTO_INCREMENT,
-name VARCHAR(30),
-PRIMARY KEY (id)
-);
-
-CREATE TABLE role (
-id INT NOT NULL AUTO_INCREMENT,
-title VARCHAR(30) NOT NULL,
-salary DECIMAL(10, 2) NOT NULL,
-department_id INT,
-PRIMARY KEY (id)
-);
-
-CREATE TABLE employee (
-id INT NOT NULL AUTO_INCREMENT,
-first_name VARCHAR(30) NOT NULL,
-last_name VARCHAR(30) NOT NULL,
-role_id INT,
-manager_id INT,
-PRIMARY KEY (id)
-);
 
 
 INSERT INTO department (name) 
