ad531364042987edb39949e72109fd4a8ee1eadc
andrew quenette <and1roxx43@gmail.com>
2021-05-17 18:33:40 +1000

    Added All employees function to js files

--- employee-tracker.js
+++ employee-tracker.js
@@ -2,7 +2,7 @@ const mysql = require ('mysql');
 const inquirer = require ('inquirer');
 const consoleTable = require('console.table');
 
-const connection = mysql.connection({
+const connection = mysql.createConnection({
     host: 'localhost',
 
     port: 3306,
@@ -26,7 +26,7 @@ const startTracker = () => {
         {
             type: 'list',
             name: 'choice',
-            message: 'What wpild you like to do?',
+            message: 'What would you like to do?',
             choices: [
                 'View All Employees', 
                 'Add Employee', 
@@ -37,6 +37,38 @@ const startTracker = () => {
     })
     .
     then((answer) => {
-    
-});
+        switch(answer.choice){
+            case 'View All Employees':
+                allEmployees();
+                break;
+            case 'Add Employee':
+                addEmployees();
+                break;
+            case 'Update Emplyee':
+                updateEmployees();
+                break;
+            case 'Delete Employee':
+                deleteEmployees();
+                break;
+            case 'Exit':
+                connection.end();
+                break;
+            default:
+                console.log(`Invalid action: ${answer.choice}`);
+        }
+    });
 };
+
+const allEmployees = () => {
+    connection.query("SELECT e.id, e.first_name, e.last_name, r.title, r.salary, d.name as'department', CONCAT(m.first_name,' ', m.last_name) as 'manager' FROM employee e INNER JOIN role r ON e.role_id = r.id LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN department d ON d.id = r.department_id;", (err, res) => 
+    {
+        if(err) throw err;
+
+        console.table(res);
+        startTracker();
+    });  
+}
+
+const addEmployees = () => {
+    
+}
